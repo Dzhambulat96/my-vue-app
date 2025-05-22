ea8ebbf935c9e053d661f34b6d30c0cb95d3ba75
dzhambulat96 <dmartazanov77@gmail.com>
2025-05-22 17:45:06 +0300

    first version

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+import MyHello from './components/MyHello.vue'
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://66673fced1293495.mokky.dev/items')
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+console.log(items)
+</script>
+
+<template>
+  <!--<Drawer />-->
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+    <div class="p-10">
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск"
+            />
+          </div>
+        </div>
+      </div>
+      <CardList :items="items" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Card from './Card.vue'
+defineProps({
+  items: Array,
+})
+const onClickAdd = () => {
+  alert('asdasd')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :price="item.price"
+      :title="item.title"
+      :image-url="item.imageUrl"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
